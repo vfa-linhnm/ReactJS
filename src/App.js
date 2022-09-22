e3b40ef844538119be2eb17b28a2676b3bf2e2a8
vfa-linhnmclear <linhnm@vitalify.aisa>
2022-09-22 14:10:27 +0700

    body1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from './Components/Header';
+import Body from './Components/Body';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Body/>
     </div>
   );
 }
---
+++ src/Components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/Body.css'
+const Body = () => {
+  return (
+    <div className='bodyContainer'>
+        <div className='img'/>
+        <h2 className='content'>あなたの「トクギ」が、きっとある。</h2>
+    </div>
+  )
+}
+
+export default Body
