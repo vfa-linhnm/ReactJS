4a7c84b0ebfd406996380505dc8f3aa57847684d
vfa-linhnmclear <linhnm@vitalify.aisa>
2022-09-23 16:35:38 +0700

    UI item search

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -6,6 +6,43 @@ import PatternTop from '../assets/img/pattern_bg_top.png'
 import PatternBottom from '../assets/img/pattern_bg_bottom.png'
 
 const Body=() => {
+  const array1={
+    title: '働いている方の国籍', service:
+        [
+            {id: 1, nameService: 'ベトナム'},
+            {id: 1, nameService: 'インドネシア'},
+            {id: 1, nameService: 'フィリピン'},
+            {id: 1, nameService: '中国'},
+            {id: 1, nameService: 'タイ'},
+            {id: 1, nameService: 'ミャンマー'},
+            {id: 1, nameService: 'カンボジア'},
+            {id: 1, nameService: 'その他'},
+        ]
+}
+  const array2={
+    title: 'manh linh', service:
+        [
+            {id: 1, nameService: 'ビルクリーニング業'},
+            {id: 1, nameService: '電気・電子情報関連産業'},
+            {id: 1, nameService: '飲食料品製造業'},
+            {id: 1, nameService: '広島・中国／四国'},
+            {id: 1, nameService: '福岡・九州／沖縄'},
+            {id: 1, nameService: 'nguygdfgdfgdfgen'},
+            {id: 1, nameService: 'nguygdfgdfgdfgdfgen'},
+        ]
+}
+  const array3={
+    title: 'manh linh', service:
+        [
+            {id: 1, nameService: 'nguyen'},
+            {id: 1, nameService: 'nguyen'},
+            {id: 1, nameService: 'nguyen'},
+            {id: 1, nameService: 'nguyen'},
+            {id: 1, nameService: 'nguyen'},
+            {id: 1, nameService: 'nguyen'},
+            {id: 1, nameService: 'nguyen'},
+        ]
+}
   return (
     <div className='bodyContainer'>
       <div className='img' />
@@ -13,9 +50,9 @@ const Body=() => {
       <div className='boxSelect'>
         <h2 className='boxSelect_title'>かんたん検索（Quick search）</h2>
         <div className='boxSelect_element'>
-          <ModalBody />
-          <ModalBody />
-          <ModalBody />
+          <ModalBody data={array1}/>
+          <ModalBody data={array2}/>
+          <ModalBody data={array1}/>
         </div>
         <div className='containerBoxSelect_button'>
           <button className='buttonBoxSelect buttonSearch'><img className='icon_search' src={IconSearch} />さがす</button>
--- src/Components/ModalBody.jsx
+++ src/Components/ModalBody.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import '../Styles/ModalBody.css'
-const ModalBody=({}) => {
+const ModalBody=({data}) => {
     const [open, isOpen]=React.useState(false)
 
     const _handleClick=() => {
         isOpen(v => !v)
     }
 
-    const array={
-        title: 'manh linh', service:
-            [
-                {id: 1, nameService: 'nguyen'},
-                {id: 1, nameService: 'nguyen'},
-                {id: 1, nameService: 'nguyen'},
-                {id: 1, nameService: 'nguyen'},
-                {id: 1, nameService: 'nguyen'},
-                {id: 1, nameService: 'nguyen'},
-                {id: 1, nameService: 'nguyen'},
-            ]
-    }
+    // const array={
+    //     title: 'manh linh', service:
+    //         [
+    //             {id: 1, nameService: 'nguyen'},
+    //             {id: 1, nameService: 'nguyen'},
+    //             {id: 1, nameService: 'nguyen'},
+    //             {id: 1, nameService: 'nguyen'},
+    //             {id: 1, nameService: 'nguyen'},
+    //             {id: 1, nameService: 'nguyen'},
+    //             {id: 1, nameService: 'nguyen'},
+    //         ]
+    // }
     return (
         <div>
             {/* ---------------- */}
@@ -26,14 +26,13 @@ const ModalBody=({}) => {
                 className={"containerModalBody "+(open? "expand":"")}
             >
                 <div className="upper" onClick={_handleClick}>
-                    <p>{array.title}</p>
+                    <p>{data.title}</p>
                 </div>
                 <div className="lower">
-                    {array.service.map((item, index) =>
-                        <div className='lower_item' key={index}>
-                            <input style={{width: 30, height: 30}} type='checkbox'></input>
-                            <label style={{paddingLeft: 5}} for="vehicle1">{item.nameService}</label>
-                        </div>
+                    {data.service.map((item, index) =>
+                        <label className='lableItem'>
+                            <input type='checkbox'/>
+                            {item.nameService}</label>
                     )}
                     <div>
                     </div>
